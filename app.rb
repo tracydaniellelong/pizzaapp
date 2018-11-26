528041c4ac39466f0c81b59e628a82ac93edbc98
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-26 14:19:35 -0500

    you can order 1 pizza

--- app.rb
+++ app.rb
@@ -5,19 +5,53 @@ get '/' do
 end
 
 post '/home' do
+	quantity = params[:quantity].to_s
+	delivery = params[:delivery]
+	redirect '/pizza?quantity=' + quantity + '&delivery=' + delivery
+end
+
+get '/pizza' do
+	quantity = params[:quantity]
+	delivery = params[:delivery]
+	erb :pizza, locals:{delivery: delivery, quantity: quantity}
+end
+
+post '/pizza' do
 	size = params[:size]
 	crust = params[:crust]
 	meats = params[:meats].join(',')
 	veggies = params[:veggies].join(',')
 	extra = params[:extra].join(',')
-	redirect '/pizza?size=' + size + '&crust=' + crust + '&meats=' + meats + '&veggies=' + veggies + '&extra=' + extra
+	quantity = params[:quantity].to_s
+	delivery = params[:delivery]
+	redirect '/summary?size=' + size + '&crust=' + crust + '&meats=' + meats + '&veggies=' + veggies + '&extra=' + extra
 end
 
-get '/pizza' do
+get '/summary' do
+	price = 0.00
 	size = params[:size]
+		if size == 'small'
+			price += 5.00
+		elsif size == 'medium'
+			price += 6.00
+		elsif size == 'large'
+			price += 7.00
+		end
 	crust = params[:crust]
-	meats = params[:meats].split(',')
-	veggies = params[:veggies].split(',')
-	extra = params[:extra].split(',')
-	erb :pizza
+		if crust == 'thin'
+			price += 0.50
+		elsif crust == 'regular'
+			price += 0.75
+		elsif crust == 'stuffed'
+			price += 1.00
+		else
+			redirect '/'
+		end
+	meats = params[:meats]
+	veggies = params[:veggies]
+	extra = params[:extra]
+	delivery = params[:delivery]
+	quantity = params[:quantity]
+	erb :summary, locals:{size: size, crust: crust, meats: meats, veggies: veggies, extra: extra, price: price, delivery: delivery}
 end
+
